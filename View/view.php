4962ae605a32d5729b903447a78b6971abb3a3a2
Juan Segundo <juansec@live.com.ar>
2020-09-20 00:33:53 -0300

    agregue bootstrap

--- View/view.php
+++ View/view.php
@@ -14,9 +14,10 @@ Class view{
             <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
             integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-            <title>"'.$this->titulo.'"</title>
+            <title>'.$this->titulo.'</title>
         </head>
-        <body>';
+        <body> 
+        <h1>'.$this->titulo.'</h1>';
         $this->finHtml='<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -27,7 +28,8 @@ Class view{
     function showAllItems($productos,$categorias){ 
         $this->html.='
         <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <button type="button" class="btn btn-secondary"><a href='.BASE_URL.'Categorias>Categorias</a></button>
+            <button type="button" class="btn btn-secondary"><a class="btn btn-secondary" 
+             href='.BASE_URL.'Categorias>Categorias</a></button>
             <div class="btn-group" role="group">
                 <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" 
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -41,25 +43,26 @@ Class view{
                 </div>
             </div>
         </div>';
-        $this->html.="<table>
+        $this->html.="<table class='table table-hover table-dark'>
         <thead>
             <tr>
-                <th>Producto</th>
-                <th>Precio</th>
-                <th>Stock</th>
-                <th>Categoria</th>
-                <th>Ver detalle</th>
+                <th scope='col'>Producto</th>
+                <th scope='col'>Precio</th>
+                <th scope='col'>Stock</th>
+                <th scope='col'>Categoria</th>
+                <th scope='col'>Ver detalle</th>
             </tr>
         <thead>
         <tbody>";
         foreach($productos as $producto) {
         $this->html.= "
             <tr>
-                <td>$producto->nombre</td>
+                <th scope='col'>$producto->nombre</th>
                 <td>$producto->precio</td>
                 <td>$producto->stock</td> 
                 <td>$producto->name</td> 
-                <td><button type='button'><a href=".BASE_URL."verDetalle/".$producto->id.">Detalle</a></button></td>  
+                <td><button class='btn btn-secondary' type='button'><a class='btn btn-secondary btn-lg active'
+                 href=".BASE_URL."verDetalle/".$producto->id.">Detalle</a></button></td>  
             </tr>";
         }
         $this->html.= " </tbody>    
@@ -69,24 +72,40 @@ Class view{
     } 
 
     function showDetalleItem($detalles){ 
-        $this->html.="<table>
+        $this->html.="
+        <div class='container'>
+            <div class='row'> 
+                <div class='col'>
+                    <h1> Detalle del producto </h1>   
+                </div> 
+                <div class='col'>              
+                    <button type='button' class='btn btn-outline-danger'>
+                    <a class='btn btn-outline-danger btn-lg active' href=".BASE_URL."home>Home</a></button>
+                </div>
+            </div>
+        </div>
+        <table class='table table-hover table-dark'>
         <thead>
             <tr>
-                <th>id-Producto</th>
-                <th>Producto</th>
-                <th>Descripcion</th>
-                <th>Precio</th>
-                <th>Stock</th>
-                <th>Categoria</th>
+                <th scope='col'>id-Producto</th>
+                <th scope='col'>Producto</th>
+                <th scope='col'>Descripcion</th>
+                <th scope='col'>Precio</th>
+                <th scope='col'>Stock</th>
+                <th scope='col'>Categoria</th>
             </tr>
         <thead>
         <tbody>";
-        foreach($detalles as $detalle) {
+        foreach($detalles as $detalle) { 
+        $descripcion=$detalle->descripcion; 
         $this->html.= "
             <tr>
-                <td>$detalle->id</td>
-                <td>$detalle->nombre</td>
-                <td>**</td> 
+                <th scope='row'>$detalle->id</th>
+                <td>$detalle->nombre</td>";
+                if($descripcion==null){ 
+                    $descripcion= "---";
+                }
+        $this->html.="<td>$descripcion</td> 
                 <td>$detalle->precio</td> 
                 <td>$detalle->stock</td> 
                 <td>$detalle->name</td>  
@@ -95,16 +114,26 @@ Class view{
         $this->html.= " </tbody>    
         </table>"; 
         echo $this->html; 
-        //**=$detalle->descripcion
     } 
 
     function showCategorias($categorias){ 
-        $this->html.=" <h1> Lista de categorias </h1>
-            <ul>"; 
+        $this->html.=" 
+        <div class='container'>
+        <div class='row'> 
+            <div class='col'>
+            <h1> Lista de categorias </h1>
+            </div> 
+            <div class='col'>
+            <button type='button' class='btn btn-outline-danger'>
+            <a class='btn btn-outline-danger btn-lg active' href=".BASE_URL."home>Home</a></button>
+            </div> 
+        </div>    
+            <ul class='list-group'>"; 
             foreach($categorias as $categoria){ 
-                $this->html.="<li>$categoria->name</li>";
+                $this->html.="<li class='list-group-item list-group-item-dark'>$categoria->name</li>";
             } 
-            "</ul>";
+            "</ul>
+            </div>";
             echo $this->html;
     } 
 
